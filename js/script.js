9d85036d3f5ffc5b6ac28bf8633b9e29de583a7c
Kirill Barchuk <pokyrill@ukr.net>
2023-01-09 00:36:00 +0200

    09_0-31
    слайдер почти такой как надо, ещё бы другой попробовать или ещё попробовать откатить

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-    $('.header__burger').click(function(event) {
-        $('.header__burger,.header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    })
-})
+$(document).ready(function () {
+  $(".header__burger").click(function (event) {
+    $(".header__burger,.header__menu").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+});
 
 
 
-
-
-
+var swiper = new Swiper(".mySwiper",{
+    
+});
